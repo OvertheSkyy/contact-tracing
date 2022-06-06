63720fe9e97dc40bcb13c5d0fd1f63dd0cb667b0
Gaeus Caskie <fabrogaeuscaskie@gmail.com>
2022-06-06 15:23:42 +0800

    firstCommit
    *added app title
    *inaantok pa Sir kaya di maggawa ehehe

---
+++ contact-tracing/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace contact_tracing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.appName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // appName
+            // 
+            this.appName.AutoSize = true;
+            this.appName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appName.Location = new System.Drawing.Point(12, 31);
+            this.appName.Name = "appName";
+            this.appName.Size = new System.Drawing.Size(432, 29);
+            this.appName.TabIndex = 0;
+            this.appName.Text = "COVID 19 - Health Contact Tracing App";
+            this.appName.Click += new System.EventHandler(this.appName_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 639);
+            this.Controls.Add(this.appName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label appName;
+    }
+}
+
---
+++ contact-tracing/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contact_tracing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void appName_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
